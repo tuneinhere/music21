6162346b0d0b1a54ba1985c5e06a93a545332f69
tuneinhere <abdatune@gmail.com>
2026-06-29 15:31:46 +0700

    update config.py

--- config.py
+++ config.py
@@ -30,8 +30,8 @@ _LOG_GROUP_ID = os.getenv("LOG_GROUP_ID")
 LOG_GROUP_ID = int(_LOG_GROUP_ID) if _LOG_GROUP_ID and _LOG_GROUP_ID.strip() not in ("", "0") else OWNER_ID
 
 # --- Support & Social ---
-SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "t.me/NakaiStore")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "t.me/NakaiStore")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "NakaiStore")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "NakaiStore")
 
 # --- Web Service Settings ---
 WEB_SERVICE = os.getenv("WEB_SERVICE", "False").lower() in ("true", "1", "t")
