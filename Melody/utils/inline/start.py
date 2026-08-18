58e8df7002df9e0aa4c3608ab430c76e839bcf5b
tuneinhere <abdatune@gmail.com>
2026-08-18 12:48:57 +0700

    update Melody/utils/inline/start.py

--- Melody/utils/inline/start.py
+++ Melody/utils/inline/start.py
@@ -9,13 +9,15 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true",
                 style=ButtonStyle.PRIMARY,
-                icon_custom_emoji_id="6034916581207707551"
+                icon_custom_emoji_id="5321373065660088847"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/{config.SUPPORT_GROUP}",
-                icon_custom_emoji_id="6034916581207707551")
+                icon_custom_emoji_id="6138955270530797645")
         ],
         [
-            InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", callback_data="about_page"),
+            InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", callback_data="about_page",
+              style=ButtonStyle.SUCCESS,
+              icon_custom_emoji_id="6138810384104034730"),
         ],
     ]
     return buttons
@@ -32,10 +34,10 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", callback_data="about_page"),
-            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", callback_data="owner_page"),
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", callback_data="owner_page", icon_custom_emoji_id="5445019302891638647"),
         ],
           [
-            InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help_page_1", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="6034916581207707551"),
+            InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help_page_1", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5445275347366989283"),
         ],
     ]
     return buttons
@@ -47,7 +49,7 @@ def about_panel(_):
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{config.SUPPORT_GROUP}"),
         ],
         [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper", style=ButtonStyle.DANGER)
+            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper", style=ButtonStyle.DANGER, icon_custom_emoji_id="5321532361702127913")
         ]
     ]
     return buttons
@@ -59,7 +61,7 @@ def owner_panel(_):
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.DEV_ID),
         ],
         [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper", style=ButtonStyle.DANGER),
+            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper", style=ButtonStyle.DANGER, icon_custom_emoji_id="5321532361702127913"),
         ]
     ]
     return buttons
