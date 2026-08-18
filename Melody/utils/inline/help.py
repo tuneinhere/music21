e5f463074f12c7e58e3d4bfc4ec53e7aa4f1ea3b
tuneinhere <abdatune@gmail.com>
2026-08-18 14:08:27 +0700

    update Melody/utils/inline/help.py

--- Melody/utils/inline/help.py
+++ Melody/utils/inline/help.py
@@ -9,6 +9,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.enums import ButtonStyle
 from Melody import app
 
+
 def help_pannel_page1(_, START: Union[bool, int] = None):
     return InlineKeyboardMarkup(
         [
@@ -31,16 +32,18 @@ def help_pannel_page1(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(text=_["H_B_10"], callback_data="help_callback hb10", style=ButtonStyle.PRIMARY, icon_custom_emoji_id="5321097148371058002"),
             ],
             [
-                InlineKeyboardButton(text="️Back page", callback_data="help_page_3", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5321532361702127913"))
+                InlineKeyboardButton(text="️Back Page", callback_data="help_page_3", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5321532361702127913"),
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"] if START else _["CLOSE_BUTTON"],
                     callback_data="settingsback_helper" if START else "close",
-                    style=ButtonStyle.DANGER, icon_custom_emoji_id="5323451799766582382")
+                    style=ButtonStyle.DANGER, 
+                    icon_custom_emoji_id="5323451799766582382"
                 ),
-                InlineKeyboardButton(text="️Next page", callback_data="help_page_2", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5321300317504030049")),
+                InlineKeyboardButton(text="️Next Page", callback_data="help_page_2", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5321300317504030049"),
             ],
         ]
     )
+
 
 def help_pannel_page2(_, START: Union[bool, int] = None):
     return InlineKeyboardMarkup(
@@ -58,16 +61,18 @@ def help_pannel_page2(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(text=_["H_B_20"], callback_data="help_callback hb20", style=ButtonStyle.PRIMARY, icon_custom_emoji_id="5258513401784573443"),
             ],
             [
-                InlineKeyboardButton(text="️back page", callback_data="help_page_1", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5321532361702127913"),
+                InlineKeyboardButton(text="️Back Page", callback_data="help_page_1", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5321532361702127913"),
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"] if START else _["CLOSE_BUTTON"],
                     callback_data="settingsback_helper" if START else "close",
-                    style=ButtonStyle.DANGER, icon_custom_emoji_id="5323451799766582382"
+                    style=ButtonStyle.DANGER, 
+                    icon_custom_emoji_id="5323451799766582382"
                 ),
-                InlineKeyboardButton(text="️next page", callback_data="help_page_3", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5321300317504030049"),
+                InlineKeyboardButton(text="️Next Page", callback_data="help_page_3", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5321300317504030049"),
             ],
         ]
     )
+
 
 def help_pannel_page3(_, START: Union[bool, int] = None):
     return InlineKeyboardMarkup(
@@ -86,12 +91,14 @@ def help_pannel_page3(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"] if START else _["CLOSE_BUTTON"],
                     callback_data="settingsback_helper" if START else "close",
-                    style=ButtonStyle.DANGER, icon_custom_emoji_id="5323451799766582382"
+                    style=ButtonStyle.DANGER, 
+                    icon_custom_emoji_id="5323451799766582382"
                 ),
-                InlineKeyboardButton(text="️Next Page", callback_data="help_page_1", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5323451799766582382"),
+                InlineKeyboardButton(text="️Next Page", callback_data="help_page_1", style=ButtonStyle.SUCCESS, icon_custom_emoji_id="5321300317504030049"),
             ],
         ]
     )
+
 
 def help_back_markup(_, page: int = 1):
     return InlineKeyboardMarkup(
@@ -105,6 +112,7 @@ def help_back_markup(_, page: int = 1):
             ]
         ]
     )
+
 
 def private_help_panel(_):
     return [
